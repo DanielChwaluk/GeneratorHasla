b67f906e627810f4cb4cd85ad4c90bf87abd9276
DanielChwaluk <ut_3580@zsp3zamosc.pl>
2023-09-26 12:54:02 +0200

    ff

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,13 +15,17 @@
             bool Cyfry = ChkCyfry.IsChecked;
             bool ZnakSpec = ChkCyfry.IsChecked;
             string haslo = "";
+            if(MaleLit || DuzeLit || Cyfry || ZnakSpec)
             while (haslo.Length < 20)
-            {
+            { 
                 if (MaleLit) haslo += Convert.ToChar(rdn.Next(97, 122));
                 if (DuzeLit) haslo += Convert.ToChar(rdn.Next(65, 90));
                 if (Cyfry) haslo += Convert.ToChar(rdn.Next(48, 57));
                 if (ZnakSpec) haslo += Convert.ToChar(rdn.Next(58, 64));
             }
+            char[] characters = haslo.ToCharArray();
+            characters.Reverse();   
+            haslo = new string(characters);
             LblWynik.Text = haslo;
             SemanticScreenReader.Announce(LblWynik.Text);
         }
