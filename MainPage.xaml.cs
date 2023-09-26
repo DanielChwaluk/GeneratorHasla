9505c04f9e3d4ffe505cbc21905dae44a2a8d76a
DanielChwaluk <ut_3580@zsp3zamosc.pl>
2023-09-26 12:25:43 +0200

    Dodaj pliki projektów.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace haslo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGenPasClicked(object sender, EventArgs e)
+        {
+            Random rdn = new Random();
+            bool MaleLit = ChkMaleLit.IsChecked;
+            bool DuzeLit = ChkDuzeLit.IsChecked;
+            bool Cyfry = ChkCyfry.IsChecked;
+            bool ZnakSpec = ChkCyfry.IsChecked;
+            string haslo = "";
+            while (haslo.Length < 20)
+            {
+                if (MaleLit) haslo += Convert.ToChar(rdn.Next(97, 122));
+                if (DuzeLit) haslo += Convert.ToChar(rdn.Next(65, 90));
+                if (Cyfry) haslo += Convert.ToChar(rdn.Next(48, 57));
+                if (ZnakSpec) haslo += Convert.ToChar(rdn.Next(58, 64));
+            }
+            LblWynik.Text = haslo;
+            SemanticScreenReader.Announce(LblWynik.Text);
+        }
+    }
+}
